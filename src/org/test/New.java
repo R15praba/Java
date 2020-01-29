3775be741f1ecca95d914314c1d53252a2bac28d
R15praba <prabasun9955@gmail.com>
2020-01-29 11:00:30 +0530

    X

---
+++ src/org/test/New.java
@@ -0,0 +1,16 @@
+package org.test;
+
+import java.io.ObjectInputStream.GetField;
+
+public class New {
+public static void main(String[] args) {
+String s = "Nisha";
+String s1 = "Nisha";
+
+
+System.out.println(System.identityHashCode(s));
+System.out.println(System.identityHashCode(s1));
+
+
+}
+}
